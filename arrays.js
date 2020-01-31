68b07f24775f86f96de2ab7da75c9607dd92aaa6
Your Name <you@example.com>
2020-01-30 16:17:18 -0800

    2nd translate

--- arrays.js
+++ arrays.js
@@ -12,6 +12,14 @@ function printIndices(items) {
 // 2. everyOtherItem
 function everyOtherItem(items) {
   // Replace this with your code
+  const result = []
+
+  for (const i in items) {
+    if (i % 2 === 0) {
+      result.push(items[i])
+    }
+  }
+  console.log(result)
 }
 
 
