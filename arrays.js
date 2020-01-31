7562cfe11f02750f8fe72377dc277277bb083711
Your Name <you@example.com>
2020-01-30 16:10:26 -0800

    1st translate

---
+++ arrays.js
@@ -0,0 +1,21 @@
+"use strict";
+
+
+// 1. printIndices
+function printIndices(items) {
+  for (const i in items) {
+    console.log(`${items[i]} ${i}`);
+  }
+}
+
+
+// 2. everyOtherItem
+function everyOtherItem(items) {
+  // Replace this with your code
+}
+
+
+// 3. smallestNItems
+function smallestNItems(items, n) {
+  // Replace this with your code
+}
